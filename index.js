207f3456f57b2c35ef7c511ddd5621693bfeca24
Aman'sGit <140941962+TechnoiVish@users.noreply.github.com>
2024-03-04 21:41:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// var typed = new Typed('#welcome',{
+//     "strings": [" Web developer","More professional", "Perfect in every way.", "Astonishing."],
+//   "typeSpeed": 50,
+//         "loop": true,
+//         "backSpeed": 25,
+//         "backDelay": 1500
+//  });
+ var typed = new Typed('#welcome',{
+  strings: ['More professional', 'Perfect in every way', 'Astonishing','Software Engineer','Programmer', 'Web developer'],
+  "typeSpeed": 50,
+  "loop": true,
+
+});
